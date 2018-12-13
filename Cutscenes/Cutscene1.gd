5d3bdaf27983a56d603c2308c73e3b999f7ea16a
JoaoNSE <nobreenrique@hotmail.com>
2018-12-13 17:30:58 -0200

    Cutscene Inicial

--- Cutscenes/Cutscene1.gd
+++ Cutscenes/Cutscene1.gd
@@ -1,4 +1,4 @@
-extends MarginContainer
+extends Node2D
 
 # class member variables go here, for example:
 # var a = 2
@@ -7,16 +7,12 @@ extends MarginContainer
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	$Container/VBoxContainer/Start.grab_focus()
+	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
-
-func _on_Start_pressed():
-	get_tree().change_scene("res://Cenas/Cutscene1.tscn")
-	
-func _on_Exit_pressed():
-	get_tree().quit()
+func start_game():
+	get_tree().change_scene("res://Cenas/Main.tscn")
--- MapLoop.gd
+++ MapLoop.gd
@@ -21,12 +21,14 @@ func _ready():
 
 func _process(delta):
 	if player.global_position.x > init.global_position.x and player.global_position.x < end.global_position.x:
-		
+		var posLocal = global_position.x - init.global_position.x
+		var posOutro = oMap.global_position.x - oMap.get_node("Init").global_position.x
 		var tamLocal = abs(end.position.x - init.position.x)
 		var tamOutro = oMap.get_node("Init").global_position.x - oMap.get_node("End").global_position.x
 		if player.global_position.x <= init.global_position.x + tamLocal/2:
 			#player.get_node("Sprite").modulate = Color(0, 0, 0)
 			oMap.position = Vector2(global_position.x + tamOutro, oMap.global_position.y)
+			#oMap.position = Vector2(global_position.x + tamOutro, oMap.global_position.y)
 		if player.global_position.x > init.global_position.x + tamLocal/2:
 			#player.get_node("Sprite").modulate = Color(1, 1, 1)
 			oMap.position = Vector2(global_position.x + tamLocal, oMap.global_position.y)
